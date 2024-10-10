ddf4b7eb2310d0446675353c40aaee8b8090ec53
Siswadi24 <danaperdanaputra32@gmail.com>
2024-10-10 12:53:17 +0700

    feat: implement navbar, configuration, dan hero section

---
+++ app/components/BaseNavigation.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <header class="py-4 border-b">
+            <nav class="flex container items-center">
+                <NuxtLink to="/" class="flex gap-1 items-center">
+                    <NuxtImg width="56px" class="w-14" src="/icon-green.png" format="webp" alt="" />
+                    <span class="text-3xl font-bold">IboyyNuxtCipes</span>
+                </NuxtLink>
+                <ul class="flex gap-6 ml-auto text-xl font-bold capitalize">
+                    <li>
+                        <NuxtLink to="/">Home</NuxtLink>
+                    </li>
+                    <li>
+                        <NuxtLink to="/about">About</NuxtLink>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    </div>
+</template>
+
+<script setup lang="ts">
+</script>
---
+++ app/pages/about.vue
@@ -0,0 +1,15 @@
+<template>
+    <main>
+        <section class="container py-20">
+            <h1 class="text-6xl font-extrabold mb-6 text-balance">About IboyyNuxtCipes</h1>
+            <p class="text-xl font-extrabold mb-6 text-balance max-w-screen-lg">
+                IboyyNuxtCipes is a recipe application based on the dummyJSON API, created by John Komarnicki for
+                the <span class="font-bold">Build Modern Apps with Iboyy Nuxt</span> Recipes.
+            </p>
+        </section>
+    </main>
+</template>
+
+<script setup lang="ts">
+
+</script>
